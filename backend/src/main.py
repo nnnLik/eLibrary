5ce68485de92a623bdf8ee67072211a57db57de0
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-12 14:40:07 +0300

    [EDIT] allowed methods and headers

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,8 +18,14 @@ def setup_settings(app):
         CORSMiddleware,
         allow_origins=origins,
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+        allow_headers=[
+            "Content-Type",
+            "Set-Cookie",
+            "Access-Control-Allow-Headers",
+            "Access-Control-Allow-Origin",
+            "Authorization",
+        ],
     )
 
 
