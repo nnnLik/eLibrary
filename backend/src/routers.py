1d8c4d1d16f532a6ce750756ef75706e943841d5
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-14 23:10:01 +0300

    [EDIT] auth configs

--- backend/src/routers.py
+++ backend/src/routers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.auth.config import auth_backend, fastapi_users
+from src.auth.auth_config import auth_backend, fastapi_users
 from src.auth.schemas import UserCreate, UserRead
 from src.auth.views import router as user_router
 
