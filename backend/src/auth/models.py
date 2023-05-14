6404c58e04fd9573e8b5e807a01b582b41bb854a
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-14 23:10:18 +0300

    [EDIT] relationship in books model

--- backend/src/auth/models.py
+++ backend/src/auth/models.py
@@ -24,7 +24,6 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
     is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    # library = relationship("Library", uselist=False, back_populates="user")
 
     def __repr__(self):
         return f"<User: {self.username}>"
--- backend/src/library/models/book.py
+++ backend/src/library/models/book.py
@@ -1,18 +1,26 @@
 from datetime import datetime
 from enum import Enum
-from uuid import uuid4
 
 from config.database import Base
 
-from sqlalchemy import Column, Date, ForeignKey, Integer, String
+from fastapi_users_db_sqlalchemy.generics import GUID
+
+from sqlalchemy import (
+    Column,
+    Date,
+    ForeignKey,
+    ForeignKeyConstraint,
+    Integer,
+    String,
+)
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class BookStatus(str, Enum):
-    unread = 'unread'
-    reading = 'reading'
-    read = 'read'
+    unread = "unread"
+    reading = "reading"
+    read = "read"
 
 
 class Book(Base):
@@ -22,7 +30,7 @@ class Book(Base):
     title: Mapped[str] = Column(String(150), nullable=False)
     published_at: Mapped[datetime] = mapped_column(Date(), nullable=False)
     status: Mapped[BookStatus] = Column(String(30), nullable=False)
-    library_id: Mapped[int] = Column(Integer, ForeignKey('el_library.id'))
+    library_id: Mapped[int] = Column(Integer, ForeignKey("el_library.id"))
 
     library = relationship("Library", back_populates="books")
     user_books = relationship("UserBook", back_populates="book")
@@ -35,22 +43,18 @@ class UserBook(Base):
     __tablename__ = "el_user_book"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    library_id: int = Column(Integer, ForeignKey('el_library.id'), nullable=False)
-    book_id: int = Column(Integer, ForeignKey('el_book.id'), nullable=False)
+    book_id: int = Column(Integer, ForeignKey("el_book.id"), nullable=False)
+    book: Mapped[Book] = relationship("Book")
 
 
 class Library(Base):
     __tablename__ = "el_library"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_id = Column('user_id', UUID, ForeignKey('el_user.id'), unique=True, index=True)
-
-    user = relationship("User", back_populates="el_library")
-    user_books = relationship("UserBook", back_populates="el_library")
-
-    def __init__(self, user_id: UUID):
-        self.id = uuid4()
-        self.user_id = user_id
+    user_id: UUID = Column(GUID, ForeignKey("el_user.id"), nullable=False)
+    user_books: int = Column(Integer, ForeignKey("el_user_book.id"), nullable=False)
+    user = relationship("User", uselist=False)
+    user_books: Mapped[UserBook] = relationship("UserBook")
 
     def __repr__(self):
         return f"User {self.user_id}"
