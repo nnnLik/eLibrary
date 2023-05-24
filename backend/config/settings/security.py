15a5d833a6ed600d635853a1459a49ac755bb4a7
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-24 12:53:21 +0300

    [ADD] cors

--- backend/config/settings/security.py
+++ backend/config/settings/security.py
@@ -30,3 +30,12 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+    "http://localhost:8081",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:4200",
+    "http://localhost:1313",
+    "http://localhost:4200",
+]
