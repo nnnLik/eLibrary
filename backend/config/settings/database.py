6bca3307e116f6aad845c17ec91ad8576113db7c
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-24 12:53:27 +0300

    [EDIT] black

--- backend/config/settings/database.py
+++ backend/config/settings/database.py
@@ -2,12 +2,12 @@ from decouple import config
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': config('DB_ENGINE', ''),
-        'NAME': config('POSTGRES_DB', ''),
-        'USER': config('POSTGRES_USER', ''),
-        'PASSWORD': config('POSTGRES_PASSWORD', ''),
-        'HOST': config('POSTGRES_HOST', ''),
-        'PORT': config('POSTGRES_PORT', ''),
+    "default": {
+        "ENGINE": config("DB_ENGINE", ""),
+        "NAME": config("POSTGRES_DB", ""),
+        "USER": config("POSTGRES_USER", ""),
+        "PASSWORD": config("POSTGRES_PASSWORD", ""),
+        "HOST": config("POSTGRES_HOST", ""),
+        "PORT": config("POSTGRES_PORT", ""),
     }
 }
--- backend/config/settings/logging.py
+++ backend/config/settings/logging.py
@@ -1,21 +1,19 @@
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt': "%d/%b/%Y %H:%M:%S",
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            "datefmt": "%d/%b/%Y %H:%M:%S",
         },
-        'simple': {'format': '%(levelname)s %(message)s'},
+        "simple": {"format": "%(levelname)s %(message)s"},
     },
-    'handlers': {
-        "console": {"class": "logging.StreamHandler", "formatter": "verbose"}
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'propagate': True,
-            'level': "INFO",
+    "handlers": {"console": {"class": "logging.StreamHandler", "formatter": "verbose"}},
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "propagate": True,
+            "level": "INFO",
         }
     },
 }
