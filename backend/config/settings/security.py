5b9a20a9b68712bfc8b7776bdbb357ae1b4a0d9a
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-26 15:42:17 +0300

    [ADD] setup django ssl

--- backend/config/settings/security.py
+++ backend/config/settings/security.py
@@ -1,3 +1,8 @@
+import os
+
+from .base import BASE_DIR
+
+
 SECRET_KEY = "django-insecure-l^i#377)m(@y&rv0qfpbo&uru_#^zo6nt)*mvroi@xx671=nf#"
 
 DEBUG = True
@@ -39,3 +44,19 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:1313",
     "http://localhost:4200",
 ]
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SSL_KEY_PATH = os.path.join(BASE_DIR, 'certs', 'key.pem')
+SSL_CERTIFICATE_PATH = os.path.join(BASE_DIR, 'certs', 'cert.pem')
+
+SERVER_SETTINGS = {
+    'BIND_ADDRESS': ('your_domain', 8888),
+    'SSL_CERTIFICATE': SSL_CERTIFICATE_PATH,
+    'SSL_PRIVATE_KEY': SSL_KEY_PATH,
+    'WSGI_APPLICATION': 'your_project.wsgi:application',
+}
+
+USE_SSL = True
